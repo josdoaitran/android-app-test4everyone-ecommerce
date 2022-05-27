eb22d696e3bc332904c20fbfb8800c585215eaed
Doai Tran <josdoaitran@gmail.com>
2022-05-27 07:46:35 +0700

    update app logo and rename app

---
+++ app/src/main/java/com/testing4everyone/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.testing4everyone.shopping.Prevalent;
+
+import com.testing4everyone.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
